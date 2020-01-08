9c2e26f210b4e425cbe666550141dd277413ea1e
RiG <Prashant.mauryaa@gmail.com>
2020-01-08 11:40:50 +0530

    this is first commit

---
+++ app/src/main/java/com/example/theeventapp/LoginNsignup/Login.java
@@ -0,0 +1,16 @@
+package com.example.theeventapp.LoginNsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.theeventapp.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
